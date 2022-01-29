0252bf0bd0e2334507d2913b8e9282f54af40872
tana3n <tana3n@gmail.com>
2022-01-29 23:02:34 +0900

    FakeWave2Loas: Improvement progress preview

--- FakeWave2Loas/FakeWave2Loas.cpp
+++ FakeWave2Loas/FakeWave2Loas.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 
 void FakeWave2Loas(const char* source) {
     uintmax_t size = filesystem::file_size(source);
-    std::cout << "[Info]Filesize: " << size << " bytes\n";
+    std::cout << "[Info]Filesize: " << double(size)/1024/1024<< " Mbytes\n";
 
     std::ifstream import_fakelatm(source, std::ios::in | std::ios::binary);
     if (!import_fakelatm) {
@@ -53,22 +53,27 @@ void FakeWave2Loas(const char* source) {
     while (import_fakelatm.tellg() < size) {
         import_fakelatm.seekg(i);
         import_fakelatm.read(hBuf, 6);
-        if ((hBuf[0] != 0x56) & ((hBuf[1] & 0xE0) != 0xe0)) {
-            //std::cout << "hBuf["<< i<<"] is not 0x56. This byte is 0x" << std::hex << (unsigned int)(unsigned char)hBuf[0] << std::endl;
+        if ((hBuf[0] != 0x56)) {
+            //std::cout << "hBuf[" << i << "] is not 0x56. This byte is 0x" << std::hex << (unsigned int)(unsigned char)hBuf[0] << std::endl;
+            i = 1 +(int)i;
+            //std::cout << "Next Byte is " << i  << std::endl;
+
+            continue;
+        }
+        if ((hBuf[1] & 0xE0) != 0xe0) {
             i = 1 + i;
             continue;
         }
         int length = ((((((unsigned char*)hBuf)[1] & 0x1F) << 8) | ((unsigned char*)hBuf)[2]) + 3);
-        //std::cout << "This Sample length is " << length << " bytes" << std::endl;
-
         char* fBuf = new char[length];
         import_fakelatm.seekg(i);
         import_fakelatm.read(fBuf, length);
         output_wav.write(fBuf, length);
-        std::cout << "\rOutput " << i + length << "bytes";
+        std::cout << "\r[" << std::setfill('0') << std::left <<std::setw(4) <<  std::floor(double(i+length) / (double)size*10000)/100 
+            << "%]";//Output " << double((size_t)i + length)/1024/1024 << "Mbytes" ;
 
         i = i + length;
-
+       // if (i >= 10000) { break; }
     }
 
 }
