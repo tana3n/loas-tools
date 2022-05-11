1bbb5ac9935acf4d02d083dc84e633e10e062a59
tana3n <tana3n@gmail.com>
2022-05-11 13:01:36 +0900

    headerが消えていたのでここだけrevert

---
+++ src/Loas2FakeWave.h
@@ -0,0 +1,29 @@
+#pragma once
+
+int Header = 56;
+struct _riffchunk {
+	uint32_t chunkId;// RIFF
+	uint32_t chunkSize;// WAVE以降の総ファイルサイズ(=合計値-8b)
+};
+struct _wavechunk {
+	uint32_t chunkId;// WAVE Headerchunk
+	uint32_t fmt; // "fmt "
+	uint32_t chunkSize; //
+	uint16_t formatTag; //PCM == 1
+	uint16_t channel; // Channel
+	uint32_t samplingRate;
+	uint32_t byteRate; // SamplingRate * ch * (Bit/8) = bytes/s
+	uint16_t blockSize; // ch * (Bit/8)
+	uint16_t bitdepth;
+};
+
+struct _datachunk {
+	uint32_t chunkId;// data
+	uint32_t chunkSize;// -126b 
+};
+
+struct _waveheader {
+	_riffchunk riff;// RIFF
+	_wavechunk wave;// WAVE
+	_datachunk data;// data
+};
